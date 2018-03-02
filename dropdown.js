0c4bdce456eb475856377623da6fef9837b62c60
twoolardFreelance <t.woolardfreelance@gmail.com>
2018-03-02 17:38:43 -0600

    link

--- dropdown.js
+++ dropdown.js
@@ -2,3 +2,9 @@ $(".dropdown-menu li a").click(function(){
   var selText = $(this).text();
   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
 });
+
+function newPage(num) {
+var url=new Array();
+url[0]="content.html";
+window.location=url[num];``
+}
